50de0fa3c51cc9b1ea9e5b8eaa3b06b99512e19a
theopappas17 <theopappas17@gmail.com>
2022-06-01 22:17:06 -0400

    Added dependencies

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,13 @@
 // TODO: Include packages needed for this application
-// Need the 
+
+// Need the File System module built into Node.js
+const fs = require('fs');
+
+//Need Inquirer module that is part of npm project
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// Started writing questions using the syntax required by Inquirer
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
