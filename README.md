cd6e2ec0569b561b9dbc2c3afba76f7069d574ff
theopappas17 <theopappas17@gmail.com>
2022-06-02 22:52:36 -0400

    Close. Need to debug

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
-#README
-#THE ${project} PROJECT
-
-##Table of Contents
-
-
-###About
-Hello. My name is ${author}. I have created the ${project} project. 
-
-
-###Questions
-If you have any questions about my project, you can reach me through github at ${gitaddress} or through e-mail at ${emailaddress}.
-
-###Licenses
-The following license is in force for this project: ${license}.
+# bootcamp-module-09-challenge
+For this challenge, we are to create an application with Node.js that takes in input from a user and generates a generic Readme that the user can then use as a starter base for future projects.
--- index.js
+++ index.js
@@ -15,15 +15,36 @@ inquirer
       message: "Who is the author(s) of this project?",
       name: "author",
     },
+    {
+        type: "input",
+        message: "What is the name of this project?",
+        name: "project",
+      },
+      {
+        type: "input",
+        message: "What is your github address?",
+        name: "gitaddress",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "emailaddress",
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What license shall you use?",
+      },
   ])
   .then((data) => {
     writeToFile(data);
   });
 
 // TODO: Create a function to write README file
-function writeToFile("README.md", md, () => {
-  console.log(data);
-}
+fs.writeToFile("GENERICREADME.md", md, (err) => {
+    if (err) throw err;
+  console.log("README generated.")
+})
 
 // // TODO: Create a function to initialize app
 // function init() {}
